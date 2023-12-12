1dc3017722d59f32084a6152446308518a3ca3e7
dinesh <dlaller1988@gmail.com>
2023-12-12 14:43:23 +0530

    Removed commented code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-// import ExploreContainer from '../components/ExploreContainer';
-// import './Home.css';
-
-// const Home: React.FC = () => {
-//   return (
-//     <IonPage>
-//       <IonHeader>
-//         <IonToolbar>
-//           <IonTitle>Blank</IonTitle>
-//         </IonToolbar>
-//       </IonHeader>
-//       <IonContent fullscreen>
-//         <IonHeader collapse="condense">
-//           <IonToolbar>
-//             <IonTitle size="large">Blank</IonTitle>
-//           </IonToolbar>
-//         </IonHeader>
-//         <ExploreContainer />
-//       </IonContent>
-//     </IonPage>
-//   );
-// };
-
-// export default Home;
-
-
 // Install necessary packages if not already installed
 // npm install @ionic/react @ionic/react-router react-router react-router-dom axios
 
